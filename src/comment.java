405b32de43449463fa7bad0a4c8e82a189b48275
soyoungmoon <dalso51166868@gmail.com>
2025-05-16 19:37:07 +0900

    feat: 주석

---
+++ src/comment.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("Hello java1"); //hello java1을 출력합니다.
+       // System.out.println("Hello java2");
+        /*System.out.println("Hello java3");
+        System.out.println("Hello java4");
+         */
+    }
+}
