3ff60b3c5bb3bc55609a6efdf51b3e0aafe45295
soyoungmoon <dalso51166868@gmail.com>
2025-05-19 14:37:48 +0900

    feat: While-break

--- src/loop/Break3.java
+++ src/loop/Break3.java
@@ -1,18 +1,15 @@
 package loop;
 
-public class Break1 {
+public class Break3 {
     public static void main(String[] args) {
         int sum = 0;
-        int i = 1;
 
-        while(true){
-            sum = sum + i;
+        for ( int i = 1; ; i++) {
+            sum += i;
             if (sum > 10) {
                 System.out.println("합이 10보다 크면 종료:" + i + " sum = " + sum);
                 break; //break 없으면 무한 반복
             }
-            i++;
         }
-
     }
 }
